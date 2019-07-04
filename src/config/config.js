9149c6653bebaa2374bb99045d89ad06b6dcd485
高炜 <1657919948@qq.com>
2019-07-04 16:08:34 +0800

    add test reserve pay test

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,6 @@
 // default config
 module.exports = {
-  workers: 1
+  workers: 1,
+  linePayChannelId: '1594623390',
+  linePayChannelSecret: '0be4c40e1180c83f6a836df6b52e58e8'
 };
--- src/controller/index.js
+++ src/controller/index.js
@@ -1,7 +1,48 @@
 const Base = require('./base.js');
+const rp = require("request-promise");
+const sandboxBaseUrl = `https://sandbox-api-pay.line.me`;
+const _ = require("lodash");
 
 module.exports = class extends Base {
   indexAction() {
     return this.display();
+  }
+  /* 请求line获取测试支付url */
+  async askPaymentUrlAction() {
+    let options = {
+      method: 'POST',
+      uri: sandboxBaseUrl + '/v2/payments/request',
+      body: JSON.stringify({
+        'productName': 'testProduct',
+        'amount': 30,
+        'currency': 'JPY',
+        'mid': 'godvzzz',
+        'confirmUrl': 'http://47.240.11.102:8360',
+        'orderId': this.genOrderId()
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'X-LINE-ChannelId': think.config('linePayChannelId'),
+        'X-LINE-ChannelSecret': think.config('linePayChannelSecret'),
+      }
+    }
+    const result = await rp(options);
+    console.log("request result:", result)
+    return this.success({ code: 200, result });
+  }
+  genOrderId() {
+    const date = new Date();
+    let orderNo =
+      "lineorder" +
+      date.getFullYear() +
+      _.padStart(date.getMonth() + 1, 2, "0") +
+      _.padStart(date.getDate(), 2, "0") +
+      _.padStart(date.getHours(), 2, "0") +
+      _.padStart(date.getMinutes(), 2, "0") +
+      _.padStart(date.getSeconds(), 2, "0") +
+      Math.random()
+        .toString(36)
+        .substr(3, 7);
+    return orderNo;
   }
 };
