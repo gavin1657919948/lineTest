07324315bc6378be4fa82240a902ad88dd140db4
高炜 <1657919948@qq.com>
2019-07-24 18:57:56 +0800

    fix

--- src/controller/notify.ts
+++ src/controller/notify.ts
@@ -1,6 +1,6 @@
 const Base = require('./base.js');
 module.exports = class extends Base {
-    async index() {
+    async indexAction() {
         console.log("notify .... pay log", this.ctx)
         return `<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>`;
     }
